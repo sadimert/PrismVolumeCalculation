1f23c4c89bf4f2df43c94733da470f7af9fa3377
sadimert <sadimertyl@gmail.com>
2021-01-29 14:05:14 +0300

    github

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	int a, b, c, k;
+	float hacim, alan;
+	
+	printf("Enter a=");
+	scanf("%d",&a);
+	
+	printf("Enter b=");
+	scanf("%d",&b);
+	
+	printf("Enter c=");
+	scanf("%d",&c);
+	
+	printf("Enter k=");
+	scanf("%d",&k);
+	
+	alan=(2*(sqrt(a*b+b*c*+a*c)));
+	
+	printf("sonuc %f",alan);
+	
+	
+	
+	
+	return 0;
+}
